92df5b2d3e9e43894a0681b586062b8b2d02ec5c
Kartik Sarangmath <kartiksarangmath@gmail.com>
2020-05-29 00:58:56 -0500

    fix

--- experiment.py
+++ experiment.py
@@ -19,64 +19,15 @@ from torch.autograd import Variable
 from utee import selector
 from boxprop_optimized import *
 import csv
-
-num_gpu = 1 if torch.cuda.is_available() else 0
-
-# load the models
 from dcgan_cifar import Generator
 
-G = Generator(ngpu=1).eval()
 
-# load weights
-G.load_state_dict(torch.load('weights_cifar/netG_epoch_199.pth',map_location=torch.device('cpu')))
-if torch.cuda.is_available():
-    G = G.cuda()
-
-batch_size = 1
-latent_size = 100
-
-model_raw, ds_fetcher, is_imagenet = selector.select('cifar10')
-
-model_raw.eval()
-
-torch.manual_seed(0)
-
-batch_size = 1
-latent_size = 100
+def main:
+	
 
 def prolip(upper_bound, lower_bound):
-	print(0)
-	tic = time.perf_counter()
-	a_o = Box_o(upper_bound,lower_bound,False)
-	a_o.convTranspose2d(weight=G.state_dict()['main.0.weight'], c_out=512, kernel_size=4, stride=1, padding=0, output_padding=0)
-	a_o.batchNorm2d(mean=G.state_dict()['main.1.running_mean'], var=G.state_dict()['main.1.running_var'], eps=1e-05, weight=G.state_dict()['main.1.weight'], bias=G.state_dict()['main.1.bias'])
-	a_o.relu()
-	print(time.perf_counter()-tic)
-	print(1)
-	tic = time.perf_counter()
-	a_o.convTranspose2d(weight=G.state_dict()['main.3.weight'], c_out=256, kernel_size=4, stride=2, padding=1, output_padding=0)
-	a_o.batchNorm2d(mean=G.state_dict()['main.4.running_mean'], var=G.state_dict()['main.4.running_var'], eps=1e-05, weight=G.state_dict()['main.4.weight'], bias=G.state_dict()['main.4.bias'])
-	a_o.relu()
-	print(time.perf_counter()-tic)
-	print(2)
-	tic = time.perf_counter()
-	a_o.convTranspose2d(weight=G.state_dict()['main.6.weight'], c_out=128, kernel_size=4, stride=2, padding=1, output_padding=0)
-	a_o.batchNorm2d(mean=G.state_dict()['main.7.running_mean'], var=G.state_dict()['main.7.running_var'], eps=1e-05, weight=G.state_dict()['main.7.weight'], bias=G.state_dict()['main.7.bias'])
-	a_o.relu()
-	print(time.perf_counter()-tic)
-	print(3)
-	tic = time.perf_counter()
-	a_o.convTranspose2d(weight=G.state_dict()['main.9.weight'], c_out=64, kernel_size=4, stride=2, padding=1, output_padding=0)
-	a_o.batchNorm2d(mean=G.state_dict()['main.10.running_mean'], var=G.state_dict()['main.10.running_var'], eps=1e-05, weight=G.state_dict()['main.10.weight'], bias=G.state_dict()['main.10.bias'])
-	a_o.relu()
-	print(time.perf_counter()-tic)
-	print(4)
-	tic = time.perf_counter()
-	a_o.convTranspose2d(weight=G.state_dict()['main.12.weight'], c_out=3, kernel_size=1, stride=1, padding=0, output_padding=0)
-	a_o.tanh()
-	print(time.perf_counter()-tic)
-	print(5)
-	print("done box prop")
+	
+
 
 	C = model_raw
 	print(0)
